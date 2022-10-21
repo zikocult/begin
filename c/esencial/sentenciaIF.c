707f6d2ebfd943e9bba2316a594d84f368ad20f3
zikocult <gbarulls@gmail.com>
2022-10-21 21:17:59 +0200

    Mejora estilo

--- c/esencial/sentenciaIF.c
+++ c/esencial/sentenciaIF.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-int main (int argc, char *argv[])
-{
-  float a,b;
-  printf("Introduzca dos números a analizar si su suma es mayor, menor o igual a 5\n");
-  scanf("%f %f",&a,&b);
+int main(int argc, char *argv[]) {
+  float a, b;
+  printf("Introduzca dos números a analizar si su suma es mayor, menor o igual "
+         "a 5\n");
+  scanf("%f %f", &a, &b);
 
-  if (a+b==5) {
+  if (a + b == 5) {
     printf("El resultado es por el culo te la hinco\n");
-  }else if (a+b<5) {
+  } else if (a + b < 5) {
     printf("El resultado es menor que 5\n");
-  }else {
+  } else {
     printf("El resultado es mayor que 5\n");
   }
-  
+
   return 0;
 }
