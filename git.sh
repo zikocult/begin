4f164dca346ce26bef56bff3d94fc942c060fd2c
zikocult <gbarulls@gmail.com>
2023-10-27 20:40:35 +0200

    Documentation

--- git.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-git add .
-git commit -m "Automatizado"
-git push -u origin main
--- ssh.sh
+++
@@ -1,3 +0,0 @@
-ssh-keygen -t ed25519 -C "example@dominio"
-eval $(ssh-agent -s)
-ssh-add ~/.ssh/id_ed25519
