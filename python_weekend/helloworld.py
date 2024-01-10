b2570eac4c59800085b1d0ccd6a7bc87606dcff3
Ziko Cult <gbarulls@gmail.com>
2024-01-10 08:08:33 +0100

    Testing

--- python_weekend/helloworld.py
+++ python_weekend/helloworld.py
@@ -6,23 +6,23 @@ print("Los monty python son", 6, "miembros", sep="*")
 print("Los monty python son", miembros, "miembros", end="*")
 print("\nsep = separador, end = final")
 
-pa_dentro = input("Introduzca algo: ")
-print("El resultado de lo introducido es:", pa_dentro)
-
-print("Hola ¿como te llamas?")
-nombre = input()
-print("Tu nombre es:", nombre)
-
-print("True or false:", 8 == 9)
-print("True or false:", 8 < 9 or 5 > 5)
-print("True or false:", 8 < 9 and 5 > 5)
-print("True or false:", 8 < 9 and 5 >= 5)
-print("True or false:", not (8 < 9 and 5 > 9))
-
-sumando1 = int(input("Introduzca el sumando1:"))
-sumando2 = int(input("Introduzca el sumando2:"))
-print("EL resultado de la suma es =", sumando1 + sumando2)
-
-dividendo1 = float(input("Introduzca el dividendo1:"))
-dividendo2 = float(input("Introduzca el dividendo2:"))
-print("El resultado de la división es =", dividendo1 / dividendo2)
+# pa_dentro = input("Introduzca algo: ")
+# print("El resultado de lo introducido es:", pa_dentro)
+#
+# print("Hola ¿como te llamas?")
+# nombre = input()
+# print("Tu nombre es:", nombre)
+#
+# print("True or false:", 8 == 9)
+# print("True or false:", 8 < 9 or 5 > 5)
+# print("True or false:", 8 < 9 and 5 > 5)
+# print("True or false:", 8 < 9 and 5 >= 5)
+# print("True or false:", not (8 < 9 and 5 > 9))
+#
+# sumando1 = int(input("Introduzca el sumando1:"))
+# sumando2 = int(input("Introduzca el sumando2:"))
+# print("EL resultado de la suma es =", sumando1 + sumando2)
+#
+# dividendo1 = float(input("Introduzca el dividendo1:"))
+# dividendo2 = float(input("Introduzca el dividendo2:"))
+# print("El resultado de la división es =", dividendo1 / dividendo2)
