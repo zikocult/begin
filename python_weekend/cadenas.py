07eaa40505423866f8a5e42d0a71fc7600cbfb05
Ziko Cult <gbarulls@gmail.com>
2024-01-17 14:06:35 +0100

    Python Weekend

---
+++ python_weekend/cadenas.py
@@ -0,0 +1,23 @@
+################## CADENAS DE TEXTO #####################
+
+cadenaejemplo = "Hola Mundo!\n"
+print(cadenaejemplo)
+print("Cambio de linea con \\n y tabulacion con \\t")
+print(
+    "Ahora si, vamos a ver las diferentes tipos de colecciones\n\t * Listas\n\t * Tuplas\n\t * Diccionarios"
+)
+
+################## LISTAS #####################
+
+listaejemplo = ["Hola", "Mundo", "!"]
+
+print("Las listas se delimitan con [] -->", listaejemplo)
+print("Con len(lista), podemos obtener el número de items =", len(listaejemplo))
+
+print("Accedemos a los items con lista[n] inciando con el 0, p.e. -->", listaejemplo[0])
+
+listanueva = ["patata", "melocoton", "tomate"]
+listafinal = listaejemplo + listanueva
+print("Las listas se pueden sumar")
+print("listafinal = listaejemplo + listanueva")
+print (listaejemplo, "+", listanueva, "=", listafinal)
