179bda457b895c3b23e023bd6ad94ec0bcc2863d
Ziko Cult <gbarulls@gmail.com>
2024-01-18 15:02:24 +0100

    Python Weekend

--- python_weekend/proba.c
+++ python_weekend/proba.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
 int main() {
-  int i, a, b, auxiliar, repes;
-  a = 0;
-  i = 0;
-  b = 1;
-  scanf("%i", &repes);
-  printf("0 ");
-  while (i < repes) {
-    printf("%i ", a);
-    auxiliar = b;
-    b = a + b;
-    a = auxiliar;
-    i++;
-  }
-  printf("\n");
-  return (0);
-};
+	int i = 0, a = 0, b = 1, auxiliar, repes;
+  
+	printf("Introduzca número de repeticiones: ");
+  	scanf("%i", &repes);
+
+  	while (i < repes) {
+    	printf("%i ", a);
+    	auxiliar = b;
+    	b = a + b;
+    	a = auxiliar;
+    	i++;
+  	}
+  	printf("\n");
+  	return (0);
+}
