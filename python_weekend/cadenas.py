2c6ae4ad6f8b40808f4e0e63512b845ef9b98c3e
Ziko Cult <gbarulls@gmail.com>
2024-01-18 08:26:38 +0100

    Python Weekend

--- python_weekend/cadenas.py
+++ python_weekend/cadenas.py
@@ -1,23 +1,24 @@
-################## CADENAS DE TEXTO #####################
+print("############## CADENAS DE TEXTO #################\n")
 
-cadenaejemplo = "Hola Mundo!\n"
+cadenaejemplo = "Hola Mundo!"
 print(cadenaejemplo)
 print("Cambio de linea con \\n y tabulacion con \\t")
+# print("\n")
 print(
-    "Ahora si, vamos a ver las diferentes tipos de colecciones\n\t * Listas\n\t * Tuplas\n\t * Diccionarios \n"
+    "\nVamos a ver las diferentes tipos de colecciones\n\t * Listas\n\t * Tuplas\n\t * Diccionarios \n"
 )
 
-################## LISTAS #####################
+print("################## LISTAS #####################\n")
 
 listaejemplo = ["Hola", "Mundo", "!"]
 
-print("Las listas se delimitan con [] -->", listaejemplo)
-print("Con len(lista), podemos obtener el número de items =", len(listaejemplo))
+print("* Las listas se delimitan con [] -->", listaejemplo)
+print("* Con len(lista), podemos obtener el número de items =", len(listaejemplo))
 
-print("Accedemos a los items con lista[n] inciando con el 0, p.e. -->", listaejemplo[0])
+print("* Accedemos a los items con lista[n] inciando con el 0, p.e. -->", listaejemplo[0])
 
 listanueva = ["patata", "melocoton", "tomate"]
 listafinal = listaejemplo + listanueva
-print("Las listas se pueden sumar")
-print("listafinal = listaejemplo + listanueva")
-print(listaejemplo, "+", listanueva, "=", listafinal)
+print("* Las listas se pueden sumar")
+print("\t- listafinal = listaejemplo + listanueva")
+print("\t-", listaejemplo, "+", listanueva, "=", listafinal)
