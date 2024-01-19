6e1c0acea17e060b7565c7aec4dcf6babc236640
Ziko Cult <gbarulls@gmail.com>
2024-01-19 13:56:17 +0100

    Python Weekend

--- python_weekend/02-cadenas.py
+++ python_weekend/02-cadenas.py
@@ -17,9 +17,8 @@ for i in range(-1, (len(cadena) * -1) - 1, -1):
     print(cadena[i], end=" ")
 
 print("\n\nCambio de linea con \\n y tabulacion con \\t")
-print(
-    "\nVamos a ver las diferentes tipos de colecciones\n\t * Listas\n\t * Tuplas\n\t * Diccionarios \n"
-)
+print("\nVamos a ver las diferentes tipos de colecciones\n")
+print("\t * Listas\n\t * Tuplas\n\t * Diccionarios \n")
 
 print("################## LISTAS #####################\n")
 
