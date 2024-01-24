8308ec74a599d2afeaad1d5803ba5cbb682bfe99
Ziko Cult <gbarulls@gmail.com>
2024-01-24 14:35:09 +0100

    Fibonacci test in C

--- python_weekend/tests/Fibonacci.c
+++ python_weekend/tests/Fibonacci.c
@@ -1,19 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fibonacci.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ZikoCult <ZikoCult>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/24 14:24:21 by ZikoCult          #+#    #+#             */
+/*   Updated: 2024/01/24 14:33:30 by ZikoCult         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 
-int main() {
-  long a = 0, b = 1, auxiliar;
-  int i = 0, repes;
+void	fibo(int repes)
+{
+	long	a;
+	long	b;
+	long	auxiliar;
+	int		i;
 
-  printf("Introduzca número de repeticiones: ");
-  scanf("%i", &repes);
+	a = 0;
+	b = 1;
+	i = 0;
+	while (i < repes)
+	{
+		printf("%i - %li\n", i, a);
+		auxiliar = b;
+		b = a + b;
+		a = auxiliar;
+		i++;
+	}
+}
 
-  while (i < repes) {
-    printf("%i - %li\n", i, a);
-    auxiliar = b;
-    b = a + b;
-    a = auxiliar;
-    i++;
-  }
-  printf("\n");
-  return (0);
+int	main(void)
+{
+	int		repes;
+
+	printf("Introduzca número de repeticiones: ");
+	scanf("%i", &repes);
+	fibo (repes);
+	return (0);
 }
--- python_weekend/tests/Fibonacci_argv.c
+++ python_weekend/tests/Fibonacci_argv.c
@@ -1,43 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fibonacci_argv.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ZikoCult <ZikoCult>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/24 14:16:48 by ZikoCult          #+#    #+#             */
+/*   Updated: 2024/01/24 14:22:35 by ZikoCult         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int fibo_rec(int repes) {
-  if (repes < 0)
-    return (-1);
-  if (repes == 0 || repes == 1)
-    return (repes);
-  else
-    return (fibo_rec(repes - 1) + fibo_rec(repes - 2));
+int	fibo_rec(int repes)
+{
+	if (repes < 0)
+		return (-1);
+	if (repes == 0 || repes == 1)
+		return (repes);
+	else
+		return (fibo_rec(repes - 1) + fibo_rec(repes - 2));
 }
 
-void fibo_sec(int repes) {
-  long a = 0, b = 1, auxiliar;
-  int i = 0;
+void	fibo_sec(int repes)
+{
+	long	a;
+	long	b;
+	long	auxiliar;
+	int		i;
 
-  while (i < repes) {
-    printf("%i - %li\n", i, a);
-    auxiliar = b;
-    b = a + b;
-    a = auxiliar;
-    i++;
-  }
-  printf("\n");
+	a = 0;
+	b = 1;
+	i = 0;
+	while (i < repes)
+	{
+		printf("%i - %li\n", i, a);
+		auxiliar = b;
+		b = a + b;
+		a = auxiliar;
+		i++;
+	}
+	printf("\n");
 }
 
-int main(int argc, char **argv) {
-  if (argc == 2 && atoi(argv[1]) > 0) {
-		printf("En este caso lo haré de forma secuencial\n\n");
-		fibo_sec(atoi(argv[1]));
-  } else if (argc < 2) {
-		printf("Sin parámetros introducidos\n");
-  } else if (atoi(argv[1]) <= 0) {
-		printf("Sin argumento válido de entrada\n");
-  } else {
-		printf("Se recoge el primer parámetro y se desechan el resto\n");
-		printf("Lo haremos de forma recursiva\n\n");
-		for (int i = 0; i < atoi(argv[1]); i++) {
-		printf("%i - %i\n", i, fibo_rec(i));
-    }
-  }
-  return (0);
+int	main(int argc, char *argv[])
+{
+	int	repes;
+	int	i;
+
+	repes = 0;
+	i = -1;
+	if (argc >= 2)
+		repes = atoi(argv[1]);
+	if (repes > 0)
+	{
+		if (argc == 2)
+		{
+			printf("En este caso lo haré de forma secuencial\n\n");
+			fibo_sec(repes);
+		}
+		else if (argc > 2)
+		{
+			printf("Se recoge el primer parámetro y se desecha el resto\n");
+			while (++i < repes)
+				printf("%i - %i\n", i, fibo_rec(i));
+		}
+	}
+	else
+		printf("Parametros introducidos incorrectos\n");
+	return (0);
 }
