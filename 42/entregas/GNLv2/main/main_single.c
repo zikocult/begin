410083c292517a96e3e26bff9143a4c8716d6d80
Guillem Baruls Casadesús <gbaruls-@student.42barcelona.com>
2024-09-05 11:10:55 +0200

    Subiendo las entregas del cursus

---
+++ 42/entregas/GNLv2/main/main_single.c
@@ -0,0 +1,29 @@
+#include "../get_next_line.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main (void)
+{
+	char	*line;
+	int		i = 0;
+	ssize_t	fd1;
+	char	file1[1024] = "./test/test3.txt";
+
+	printf("BUFFER_SIZE: %d\n----------------------\n", BUFFER_SIZE);
+	fd1 = open(file1, O_RDONLY);
+	if (fd1== -1)
+	{
+		printf("Fail!\n");
+		return (0);
+	}
+	while (i <= 12)
+	{
+		line = get_next_line(fd1);
+		printf("[fd1]: %s\n", line);
+		free(line);
+		i++;
+	}
+	close(fd1);
+	return (0);
+}
