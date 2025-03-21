88688e1dec4ed80135a37476415e6b0955db45d4
Guillem Barulls <gbarulls@gmail.com>
2025-03-21 21:30:05 +0100

    Rush00

--- 42/piscinas/segunda/Rush00/main.c
+++ 42/piscinas/segunda/Rush00/main.c
@@ -6,22 +6,22 @@
 /*   By: gbaruls- <gbaruls-@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 09:47:15 by gbaruls-          #+#    #+#             */
-/*   Updated: 2024/03/26 17:24:20 by gbaruls-         ###   ########.fr       */
+/*   Updated: 2025/03/21 21:26:20 by Guillem Barulls  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	rush(int x, int y);
 
-int	main(void)
+// int	main(void)
+// {
+// 	rush(5, -13);
+// 	return (0);
+// }
+//
+int	main(int argc, char **argv)
 {
-	rush(5, -13);
+	if (argc < 3)
+		return (0);
+	rush (argv[1][0] - 48, argv[2][0] - 48);
 	return (0);
 }
-
-/* int	main(int argc, char **argv) */
-/* { */
-/* 	if (argc < 3) */
-/* 		return (0); */
-/* 	rush (argv[1][0] - 48, argv[2][0] - 48); */
-/* 	return (0); */
-/* } */
